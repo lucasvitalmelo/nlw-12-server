411dda950bc71721e51a084d5470b3813f931ac5
Lucas Vital de Melo <lucasvitaldemelo@gmail.com>
2023-05-20 18:47:44 -0300

    aula 4

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,20 @@ import cors from "@fastify/cors";
 
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+
+import multipart from "@fastify/multipart";
 import jwt from "@fastify/jwt";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads/",
+});
 
 app.register(cors, {
   origin: true,
@@ -17,6 +28,7 @@ app.register(jwt, {
 });
 
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 app
