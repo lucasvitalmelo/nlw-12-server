a25be78c81ed48e7c9c7483218f20bd2f1a7b832
Lucas Vital de Melo <lucasvitaldemelo@gmail.com>
2023-05-20 13:36:05 -0300

    aula 3

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,29 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import { memoriesRoutes } from './routes/memories'
+import "dotenv/config";
 
-const app = fastify()
+import fastify from "fastify";
+import cors from "@fastify/cors";
+
+import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import jwt from "@fastify/jwt";
+
+const app = fastify();
 
 app.register(cors, {
-  origin: true
-})
+  origin: true,
+});
+app.register(jwt, {
+  secret: "luca-spacetime-2023-12-nlw",
+});
 
-app.register(memoriesRoutes)
+app.register(authRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
+    host: "0.0.0.0",
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running to http://localhost:3333')
-  })
+    console.log("🚀 HTTP server running to http://localhost:3333");
+  });
